c50a28cf755cbee9cb69ce7d44aa6efebf2f3849
Aidan McBride <aidankmcbride@gmail.com>
2021-08-13 12:34:26 -0400

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DragNDropComponent } from './components/drag-n-drop/drag-n-drop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DragNDropComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/drag-n-drop/drag-n-drop.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-drag-n-drop',
+  templateUrl: './drag-n-drop.component.html',
+  styleUrls: ['./drag-n-drop.component.scss']
+})
+export class DragNDropComponent implements OnInit {
+
+  public listA = ["1", "2", "3", "4"]
+  public listB = ["A", "B", "C", "D"]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public dropItem(event: any): void {
+    if (event.previousContainer === event.container) {
+      this.arrayMove(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      this.transferItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  
+  }
+
+  public transferItem(prevArr: any[], currentArr: any[], fromIndex: number, toIndex: number): void {
+    let movedItem = prevArr[fromIndex];
+    prevArr.splice(fromIndex, 1)
+    currentArr.splice(toIndex, 0, movedItem)
+  }
+
+  public arrayMove(arr: any[], fromIndex: number, toIndex: number): void {
+    let element = arr[fromIndex];
+    arr.splice(fromIndex, 1);
+    arr.splice(toIndex, 0, element);
+  }
+
+}
+
+
+// npm install @angular/cdk
+// import into app module
